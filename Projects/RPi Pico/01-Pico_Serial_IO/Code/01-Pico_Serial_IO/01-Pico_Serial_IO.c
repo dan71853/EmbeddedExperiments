aa491c55e8157504bfeda4e25a38b307509a15cc
dan71853 <dan71853@gmail.com>
2025-10-26 10:58:49 +1300

    Added interrupt to button, needs debouncing

--- Projects/RPi Pico/01-Pico_Serial_IO/Code/01-Pico_Serial_IO/01-Pico_Serial_IO.c
+++ Projects/RPi Pico/01-Pico_Serial_IO/Code/01-Pico_Serial_IO/01-Pico_Serial_IO.c
@@ -1,40 +1,36 @@
+#pragma GCC optimize ("O0") //Used to disable optimization, helpful for debugging
+
 #include <stdio.h>
-#include "pico/stdlib.h" 
-#include "hardware/adc.h"
-#include "pico/cyw43_arch.h" 
- 
+#include "pico/stdlib.h"
+#include "hardware/adc.h" 
+
 
 #define BTN_PIN 16
 #define POT_PIN 26
 #define POT_ADC_INPUT 0
 
+void buttonCallback(uint gpio, uint32_t events){
 
-int main()
-{
+    bool buttonState = gpio_get(BTN_PIN);
+
+    uint16_t potReading = adc_read();
+
+    printf("Button is %s. Pot is %d\n ", buttonState ? "released" : "pressed", potReading);
+}
+
+int main(){
     stdio_init_all();
 
     // Setup digital input
     gpio_init(BTN_PIN);
     gpio_set_dir(BTN_PIN, false);         // Set as input
     gpio_set_pulls(BTN_PIN, true, false); // Enable pullup resistor
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &buttonCallback);
 
-    //Setup analog input
+    // Setup analog input
     adc_init();
-    adc_gpio_init(POT_PIN); //Reset GPIO pin
-    adc_select_input(POT_ADC_INPUT); // Enable ADC Input 
+    adc_gpio_init(POT_PIN);          // Reset GPIO pin
+    adc_select_input(POT_ADC_INPUT); // Enable ADC Input    
 
-
-    cyw43_arch_init();//Enable onboard LED
- 
-    
-    while (true){
-         bool buttonState = gpio_get(BTN_PIN); 
-        
-         uint16_t potReading = adc_read(); 
-        
-        printf("Button is %s. Pot is %d\n ", buttonState?"released":"pressed", potReading);
-
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !buttonState); //Set onboard LED, on when button pressed
-        sleep_ms(100);
-    }
+    while (true);
 }
